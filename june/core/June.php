e34c13cebfb958516d698cbbf191b2a0889a6c0e
mnking <kingads2@outlook.com>
2014-08-15 21:19:09 +0700

    comment June class

--- june/core/June.php
+++ june/core/June.php
@@ -2,15 +2,33 @@
 
 class June {
 
+    /**
+     * Get version of June
+     *
+     * @return string
+     */
     public static function version()
     {
         return 'June v4.0.1';
     }
 
+    /**
+     * Get public url
+     *
+     * @param $path
+     * @return string
+     */
     public static function asset($path)
     {
         return asset('public/' . $path);
     }
+
+    /**
+     * Get cloud public url
+     *
+     * @param $path
+     * @return string
+     */
     public static function cloudAsset($path)
     {
         return asset('app/cloud/public/' . $path);
